f387dd2c9accd94c644f4fd1d2546de3d5c9813c
vitor <vitorfernando2004@sapo.pt>
2023-05-15 18:23:49 +0100

    funçao randomizar

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+
+main()
+
+def tabuleiro():
+    tabuleiro= list(range(30))
+
+
 import random
 
 def randomizar():
-    return random.randint(0, 4)
+    for i in range(4):
+        numeros[]
+        numero=random.randint(0, 4)
+
 
